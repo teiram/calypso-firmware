ae11bc0ce71be03dcb5756a256ba170aa1f60a2a
Manuel Teira <manuel.teira@gmail.com>
2024-09-26 20:47:03 +0200

    Preliminary HID mouse report generation

--- src/mist/MistUSBMouseHandler.cxx
+++ src/mist/MistUSBMouseHandler.cxx
@@ -10,7 +10,12 @@ using namespace calypso;
 
 void MistUSBMouseHandler::handleReport(uint8_t const* report, uint16_t len) {
     USB_DEBUG_DUMP(L_DEBUG, "Mouse report", report, len);
-    uint8_t buttons;
-    int8_t x, y, z;
-    //user_io_mouse(0, buttons, x, y, z);
+    // TODO: Check if this is right
+    if (len >= 8) {
+        uint8_t buttons = report[1];
+        int8_t x = report[2];
+        int8_t y = report[4];
+        int8_t z = report[6];
+        user_io_mouse(0, buttons, x, y, z);
+    }
 }
